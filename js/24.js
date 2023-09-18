cf94b3b22c00f1544f682ba8bd1cae56b07b16e1
Devohka <110058157+Devohka@users.noreply.github.com>
2023-09-18 20:38:47 +0300

    how

--- js/24.js
+++ js/24.js
@@ -1,3 +1,21 @@
+// 1
 const btnHow = document.querySelector("#how-btn");
 btnHow.textContent = "я змінив це";
 
+
+// 2
+const titleHow = document.querySelector("h1");
+titleHow.textContent = "Заголовок";
+
+const howImg = document.querySelector("#img-how");
+howImg.src = "./img/unnamed.png";
+
+
+// 3
+const pageHow = document.querySelector("#how-page");
+pageHow.href = "./img/images.png";
+pageHow.alt = "google";
+
+// 4
+const howLi = document.querySelector("#how-li");
+howLi.textContent = "я змінюю це";
